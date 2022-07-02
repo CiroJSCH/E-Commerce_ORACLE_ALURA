5ce54abbfb7e2c27988b5d44f7274f7514322d0b
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-07-02 18:23:31 -0300

    Info de productos
    Editada la informacion de los productos

---
+++ assets/principal/productos-main.js
@@ -0,0 +1,131 @@
+export {productosMain}
+
+const productosMain = [
+
+    {
+        'nombre' : 'Producto XYZ',
+        'imagen' : 'starWars/cascoStorm.png',
+        'precio' : '$ 60, 00',
+        'codigo' : 'Ver producto'
+    },
+
+    {
+        'nombre' : 'Producto XYZ',
+        'imagen' : 'starWars/cascoDarth.png',
+        'precio' : '$ 60, 00',
+        'codigo' : 'Ver producto'
+    },
+
+    {
+        'nombre' : 'Producto XYZ',
+        'imagen' : 'starWars/yoda.png',
+        'precio' : '$ 60, 00',
+        'codigo' : 'Ver producto'
+    },
+
+    {
+        'nombre' : 'Producto XYZ',
+        'imagen' : 'starWars/muñecoStorm.png',
+        'precio' : '$ 60, 00',
+        'codigo' : 'Ver producto'
+    },
+
+    {
+        'nombre' : 'Producto XYZ',
+        'imagen' : 'starWars/babyYoda.png',
+        'precio' : '$ 60, 00',
+        'codigo' : 'Ver producto'
+    },
+
+    {
+        'nombre' : 'Producto XYZ',
+        'imagen' : 'starWars/negro.png',
+        'precio' : '$ 60, 00',
+        'codigo' : 'Ver producto'
+    },
+
+    {
+        'nombre' : 'Control XYZ',
+        'imagen' : 'Consolas/mando.png',
+        'precio' : '$ 60, 00',
+        'codigo' : 'Ver producto'
+    },
+
+    {
+        'nombre' : 'Control y consola XYZ',
+        'imagen' : 'Consolas/play.png',
+        'precio' : '$ 60, 00',
+        'codigo' : 'Ver producto'
+    },
+
+    {
+        'nombre' : 'Control XYZ',
+        'imagen' : 'Consolas/nintendo.png',
+        'precio' : '$ 60, 00',
+        'codigo' : 'Ver producto'
+    },
+
+    {
+        'nombre' : 'Control XYZ',
+        'imagen' : 'Consolas/ninSwitch.png',
+        'precio' : '$ 60, 00',
+        'codigo' : 'Ver producto'
+    },
+
+    {
+        'nombre' : 'Control XYZ',
+        'imagen' : 'Consolas/xbox.png',
+        'precio' : '$ 60, 00',
+        'codigo' : 'Ver producto'
+    },
+
+    {
+        'nombre' : 'Game Boy Color',
+        'imagen' : 'Consolas/gameboy.png',
+        'precio' : '$ 60, 00',
+        'codigo' : 'Ver producto'
+    },
+
+    {
+        'nombre' : 'Camisa Atari',
+        'imagen' : 'Diversos/ataria.png',
+        'precio' : '$ 60, 00',
+        'codigo' : 'Ver producto'
+    },
+
+    {
+        'nombre' : 'Camisa SNES',
+        'imagen' : 'Diversos/SNES.png',
+        'precio' : '$ 60, 00',
+        'codigo' : 'Ver producto'
+    },
+
+    {
+        'nombre' : 'Producto XYZ',
+        'imagen' : 'Diversos/sonic.png',
+        'precio' : '$ 60, 00',
+        'codigo' : 'Ver producto'
+    },
+
+    {
+        'nombre' : 'Control y Conzola XYZ',
+        'imagen' : 'Diversos/reloj.png',
+        'precio' : '$ 60, 00',
+        'codigo' : 'Ver producto'
+    },
+
+    {
+        'nombre' : 'Control y Conzola XYZ',
+        'imagen' : 'Diversos/humano.png',
+        'precio' : '$ 60, 00',
+        'codigo' : 'Ver producto'
+    },
+
+    {
+        'nombre' : 'Producto XYZ',
+        'imagen' : 'Diversos/pikachu.png',
+        'precio' : '$ 60, 00',
+        'codigo' : 'Ver producto'
+    }
+
+]
